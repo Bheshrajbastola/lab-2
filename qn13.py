a3c723442222f27f2786047a4a9ae542ed6d2c5c
dell <BOB-ctrl-creator>
2021-06-26 23:52:10 +0545

    ammend

---
+++ qn13.py
@@ -0,0 +1,9 @@
+'''What is the output of ‘APPLE’ > ‘apple’?'''
+#a="APPLE"
+#b="apple"
+#print(a>b)
+
+
+a = 'APPLE'
+b = a.lower()
+print (b)
---
+++ qn14.py
@@ -0,0 +1,3 @@
+'''What is the result of float (1)?'''
+i=float(3)
+print(i)
